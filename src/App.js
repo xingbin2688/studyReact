27824750fc10f21eed4bf866fdcef3e0dae20a4f
xingbin <xingbinhaihao@163.com>
2021-03-03 16:08:24 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd-mobile';
+class App extends React.Component{
+  render() {
+    return <div>123
+      <Button>按钮</Button>
+    </div>
+  }
+}
+
+
+export default App;
